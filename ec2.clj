7e522698e365ea091963953d9d225de930a10456
Joakim L. Engeset <joakim.engeset@gmail.com>
2022-12-08 09:41:12 +0100

    Expand ec2 example

--- ec2.clj
+++ ec2.clj
@@ -6,12 +6,20 @@
                       :region "eu-west-1"
                       :credentials-provider (get-creds)}))
 
+;; Helper-function to extract interesting information from an EC2 instance
+(defn destructure [{:keys [Tags InstanceId PublicIpAddress State]}]
+  {:Name (->> Tags (filter #(= (:Key %) "Name")) (map :Value) (apply str))
+   :Id InstanceId
+   :PublicIp PublicIpAddress
+   :State (:Name State)})
+
+(defn to-base64 [s]
+  (.encodeToString (Base64/getEncoder) (.getBytes s)))
+
 (comment
   (-> (aws/ops ec2) keys sort)
 
-  (defn to-base64 [s]
-    (.encodeToString (Base64/getEncoder) (.getBytes s)))
-;; Create EC2 instance with default storage and an init-script from the project
+;; Create EC2 instance with default storage and an init-script from project
 ;; NB: doesn't configure security groups and access to public ip
   (aws/invoke ec2 {:op :RunInstances
                    :request
@@ -26,32 +34,32 @@
 
   (aws/doc ec2 :DescribeInstances)
 
-;; Helper-function to extract interesting information from an EC2 instance
-  (defn destructure [ec2]
-    {:Name (->> ec2 :Tags (filter #(= (:Key %) "Name")) (map :Value) (apply str))
-     :Id (:InstanceId ec2)
-     :PublicIp (:PublicIpAddress ec2)
-     :State (-> ec2 :State :Name)})
-
 ;;  List all instances
-  (->> (aws/invoke ec2 {:op :DescribeInstances})
-       :Reservations
-       (map :Instances)
-       (apply concat)
-       (map destructure))
-  ;; => ({:Name "my example instance", :Id "i-041a0ed0d07202bcd", :PublicIp "3.219.244.184", :State "running"}
+  (defn list-instances []
+    (->> (aws/invoke ec2 {:op :DescribeInstances})
+         :Reservations
+         (map :Instances)
+         (apply concat)
+         (map destructure)))
+  ;; => ({:Name "my example instance", :Id "i-041a0ed0d07202bdcd", :PublicIp "3.219.244.184", :State "running"}
   ;;     {:Name "my second instance", :Id "i-0d26b26bb5ca96a2c", :PublicIp nil, :State "terminated"}
 
+  (defn terminate-instances [instances]
+    (aws/invoke ec2 {:op :TerminateInstances
+                     :request {:InstanceIds instances}}))
+
 ;; List all running instances
-  (->> (aws/invoke ec2 {:op :DescribeInstances})
-       :Reservations
-       (map :Instances)
-       (apply concat)
-       (map destructure)
+  (->> (list-instances)
        (filter #(= (:State %) "running")))
 
+  ;; WARNING - Terminate all EC2 instances
+  (->> (list-instances)
+       (mapv #(:Id %))
+       (terminate-instances))
+
 ;; Delete (terminate) instance
-  (aws/invoke ec2 {:op :TerminateInstances :request {:InstanceIds ["i-039e96188898d8d95"]}})
+  (aws/invoke ec2 {:op :TerminateInstances
+                   :request {:InstanceIds ["i-039e96188898d8d95"]}})
   ;; => {:TerminatingInstances
   ;;     [{:CurrentState {:Code 32, :Name "shutting-down"},
   ;;       :InstanceId "i-038e96188898d8d95",
