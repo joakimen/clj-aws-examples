bda1fef7a05474d7005ecc91a1db5f69cb932790
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-06-30 15:52:04 +0200

    Add some sdk helper util

--- util.clj
+++ util.clj
@@ -1,9 +1,31 @@
-(require '[clojure.edn :as edn])
+(require '[clojure.edn :as edn]
+         '[clojure.string :as str])
+
+(defn get-aws-apiversions []
+  (->> (slurp "https://raw.githubusercontent.com/cognitect-labs/aws-api/main/latest-releases.edn")
+       edn/read-string
+       (map second)
+       (filter #(not (str/blank? (get-in % [:aws/serviceFullName]))))))
 
 (comment
 
-;;   get latest api versions
-  (->> "https://raw.githubusercontent.com/cognitect-labs/aws-api/main/latest-releases.edn"
-       slurp
+      ;;  (map second)
+
+
+      ;;  
+  (->> (slurp "https://raw.githubusercontent.com/cognitect-labs/aws-api/main/latest-releases.edn")
        edn/read-string
-       (into [])))
+       (filter #(not (str/blank? (get-in (second %) [:aws/serviceFullName]))))
+       (filter #(re-find #"Lambda" (:aws/serviceFullName (second %)))))
+
+  (->> (slurp "https://raw.githubusercontent.com/cognitect-labs/aws-api/main/latest-releases.edn")
+       edn/read-string
+       (filter #(not (str/blank? (get-in (second %) [:aws/serviceFullName])))))
+;;   get latest api versions
+  ;; (def resp (slurp  "https://raw.githubusercontent.com/cognitect-labs/aws-api/main/latest-releases.edn"))
+  ;; (->> resp
+  (->> (get-aws-apiversions)
+       (filter #(re-find #"S3" (:aws/serviceFullName %)))
+       (filter #(re-find #"S3" (:aws/serviceFullName %)))
+;; 
+       ))
